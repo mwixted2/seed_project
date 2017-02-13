f17608f65d59d3247dc8a16cea61f94ba20c0947
mwixted2 <mwixted2@gmail.com>
2017-02-12 20:24:16 -0500

    sometimes I can code

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ GPIO.setup(22, GPIO.OUT)
 
 
 GPIO.output(17, GPIO.LOW)
-#GPIO.output(22, GPIO.LOW)
+GPIO.output(22, GPIO.LOW)
 #GPIO.output(4, GPIO.LOW)
 #GPIO.output(5, GPIO.LOW)
 #GPIO.output(27, GPIO.LOW)
