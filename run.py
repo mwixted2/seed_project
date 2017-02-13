210872c1ba4ff80b8f0b529b78b1d793760b942c
mwixted2 <mwixted2@gmail.com>
2017-02-12 20:15:04 -0500

    changing path of video file

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
-movie1 = ("/home/pi/Videos/video1.mov")
+movie1 = ("/home/pi/seed_project/video1.mov")
 
 GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 #GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
